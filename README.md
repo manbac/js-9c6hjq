d295f2fa04865eb73b89331439ab1d9e57a38505
manbac <118624852+manbac@users.noreply.github.com>
2022-11-20 01:22:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-9c6hjq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-9c6hjq)
---
+++ index.js
@@ -0,0 +1,12 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+
+const myFirstApp = () => {
+  return  'Hello World!'
+}
+
+console.log(myFirstApp);
+
